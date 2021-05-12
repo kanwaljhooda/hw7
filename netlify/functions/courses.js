b7cf794e45fd0b49720960ca21e8e5eb5d4afb36
K.J. Hooda <kj.hooda@kellogg.northwestern.edu>
2021-05-11 22:33:34 -0500

    HW 7

--- netlify/functions/courses.js
+++ netlify/functions/courses.js
@@ -54,6 +54,13 @@ exports.handler = async function(event) {
 
   // get the data from the document
   let courseData = course.data()
+  // console.log(courseData)
+
+  // set a counter for course review count
+  courseData.courseReviewCount = 0
+
+  // set a counter for total course review ratings 
+  let courseTotalReviewRating = 0
 
   // set a new Array as part of the return value
   courseData.sections = []
@@ -65,12 +72,14 @@ exports.handler = async function(event) {
   let sections = sectionsQuery.docs
 
   // loop through the documents
-  for (let i=0; i < sections.length; i++) {
+  for (let sectionIndex=0; sectionIndex < sections.length; sectionIndex++) {
     // get the document ID of the section
-    let sectionId = sections[i].id
+    let sectionId = sections[sectionIndex].id
+    // console.log(sectionId)
 
     // get the data from the section
-    let sectionData = sections[i].data()
+    let sectionData = sections[sectionIndex].data()
+    // console.log(sectionData)
 
     // ask Firebase for the lecturer with the ID provided by the section; hint: read "Retrieve One Document (when you know the Document ID)" in the reference
     let lecturerQuery = await db.collection('lecturers').doc(sectionData.lecturerId).get()
@@ -79,13 +88,70 @@ exports.handler = async function(event) {
     let lecturer = lecturerQuery.data()
 
     // add the lecturer's name to the section's data
-    sectionData.lecturerName = lecturer.name
-
-    // add the section data to the courseData
-    courseData.sections.push(sectionData)
+    sectionData.lecturer = lecturer.lecturerName
+    // console.log(sectionData)
 
     // 🔥 your code for the reviews/ratings goes here
+    // create section object to only show lecturer and reviews
+    let sectionObject = {
+      lecturer: sectionData.lecturer,
+      reviews: []
+    }
+
+    // retrieve reviews query for this section only
+    let reviewsQuery = await db.collection('reviews').where('sectionId','==', sectionId).get()
+    
+    // get documents from the query
+    let sectionReviews = reviewsQuery.docs
+    
+    // check if there are any reviews
+    if (sectionReviews.length > 0) {
+          
+      // set counter for section reviews
+      sectionObject.sectionReviewCount = 0
+
+      // set counter for section ratings
+      let sectionTotalReviewRating = 0
+
+      // loop through the review documents
+      for (let reviewIndex = 0; reviewIndex < sectionReviews.length; reviewIndex++) {
+               
+        // get the data from the review
+        let reviewData = sectionReviews[reviewIndex].data()
+
+        // create review object that shows body and rating only
+        let reviewObject = {
+          body: reviewData.body,
+          rating: reviewData.rating
+        }
+
+        // add 1 to sectionReviewCount counter
+        sectionObject.sectionReviewCount = sectionObject.sectionReviewCount + 1
+
+        // add review rating to sectionTotalReviewRating
+        sectionTotalReviewRating = sectionTotalReviewRating + reviewData.rating
+        
+        // add review data to the section data
+        sectionObject.reviews.push(reviewObject)
+      }
+
+      // calculate section rating average
+      sectionObject.sectionAverageReviewRating = sectionTotalReviewRating / sectionObject.sectionReviewCount
+        
+      // add the section data to the courseData
+      courseData.sections.push(sectionObject)
+
+      // add the section review count to course review count
+      courseData.courseReviewCount = courseData.courseReviewCount + sectionObject.sectionReviewCount
+
+      // add the section review rating total to course review rating total
+      courseTotalReviewRating = courseTotalReviewRating + sectionTotalReviewRating            
+    } 
+
   }
+
+  // calculate course rating average
+  courseData.courseAverageReviewRating = courseTotalReviewRating / courseData.courseReviewCount
 
   // return the standard response
   return {
